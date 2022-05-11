d352bc9b03444284a0b36543e11db5a801e86085
chalmery <chaochaoycc@gmail.com>
2022-05-11 23:49:07 +0800

    一个值的可变引用和不可变引用只能存在一个

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 fn main(){
     let mut s = String::from("Hello");
-    {
-        let m1 = &mut s;
-    }
+    let m1 = & s;
+    let m2 = & s;
+    let m3 = &mut s;
 
-    let m2 = &mut s;
+    println!("{} {} {}",m1,m2,m3);
 }
