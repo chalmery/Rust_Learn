71cfa3c4e0b9942279dab326cb7ad52bf11f8d38
chalmery <chaochaoycc@gmail.com>
2022-05-11 23:38:11 +0800

    4.3-2 可变引用

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 fn main(){
-    let s1 = String::from("Hello");
-    let len = get_lenth(&s1);
+    let mut s1 = String::from("Hello");
+    let len = get_lenth(&mut s1);
 
     println!("{} length is {}",s1,len);
 }
 
-fn get_lenth(s: &String) -> usize {
+fn get_lenth(s: &mut String) -> usize {
+    s.push_str(", World");
     s.len()
 }
