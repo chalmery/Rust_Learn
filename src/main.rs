77455f50436c424c7cc597b99eb263cc8b6ba3ca
chalmery <chaochaoycc@gmail.com>
2022-05-11 23:19:10 +0800

    4.3 引用

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,10 @@
-//复合类型
+fn main(){
+    let s1 = String::from("Hello");
+    let len = get_lenth(&s1);
 
+    println!("{} length is {}",s1,len);
+}
 
-use std::collections::HashMap;
-
-fn main() {
-    //声明数组的方式
-    let aa : [i32;5] = [1,2,3,4,5];
-    //假设数组的每个元素都是相同的
-    // let aaa : [1,5];
-
-    let map = HashMap;
-
-    // //for循环
-    // let a = [10, 20, 30, 40, 50];
-    // for element in a {
-    //     print!("{} ", element);
-    // }
-    // println!(" ");
-    // for element in a.iter() {
-    //     print!("{} ", element);
-    // }
-
-    // //range
-    // for number in 1..4 {
-    //     print!("{} ", number);
-    // }
-    // println!(" ");
-    // //反转range
-    // for number in (1..4).rev() {
-    //     print!("{} ", number)
-    // }
+fn get_lenth(s: &String) -> usize {
+    s.len()
 }
