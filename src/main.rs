f8810919b0f5f461cc616447fb3b005ec883189a
chalmery <chaochaoycc@gmail.com>
2022-05-11 23:12:34 +0800

    循环

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,33 @@
 //复合类型
+
+
+use std::collections::HashMap;
+
 fn main() {
-    //元组 tuple
-    //let tup: (i32, f64, u8) = (500, 6.4, 1);
+    //声明数组的方式
+    let aa : [i32;5] = [1,2,3,4,5];
+    //假设数组的每个元素都是相同的
+    // let aaa : [1,5];
 
-    //println!("{},{},{}", tup.0, tup.1, tup.2);
+    let map = HashMap;
 
-    // 解构 tuple
-    //let (x, y, z) = tup;
+    // //for循环
+    // let a = [10, 20, 30, 40, 50];
+    // for element in a {
+    //     print!("{} ", element);
+    // }
+    // println!(" ");
+    // for element in a.iter() {
+    //     print!("{} ", element);
+    // }
 
-    //println!("{},{},{}", x, y, z);
-
-    //数组，数组是固定大小的，因此会存放在栈内存因此如果想存放在栈内存则使用数组
-    //let a = [1, 2, 3, 4, 5];
-
-    // vector
-
-    //语句表达式和函数
-    //函数的返回值默认是最后一行的表达式
-    //如果需要提前返回则需要使用return
-
-    //for循环
-    let a = [10, 20, 30, 40, 50];
-    for element in a {
-        print!("{} ", element);
-    }
-    println!(" ");
-    for element in a.iter() {
-        print!("{} ", element);
-    }
-
-    //range
-    for number in 1..4 {
-        print!("{} ", number);
-    }
-    println!(" ");
-    //反转range
-    for number in (1..4).rev() {
-        print!("{} ", number)
-    }
+    // //range
+    // for number in 1..4 {
+    //     print!("{} ", number);
+    // }
+    // println!(" ");
+    // //反转range
+    // for number in (1..4).rev() {
+    //     print!("{} ", number)
+    // }
 }
