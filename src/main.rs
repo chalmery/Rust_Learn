d1b6609580d6600549cac4dcb6b0f4638def211a
chalmery <chaochaoycc@gmail.com>
2022-05-14 12:10:01 +0800

    4.3 使用字符串切片作为入参

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main(){
-    let  str = "hello world";
+    let  s = String::from("Hello World");
+    let str = first_world(&s[..]);
     println!("{}",str);
+}
+
+fn first_world(s: &str) ->&str{
+    let bytes = s.as_bytes();
+    // iter会返回一个迭代器
+    // enumerate会将迭代器包装为一个元组，1 索引， 2 元素的引用
+    for (i,&item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return  &s[..i];
+        }
+    }
+    &s[..]
+}
