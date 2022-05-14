7f54ef434c2cb4de9404021c2cadde31dfdde6e2
chalmery <chaochaoycc@gmail.com>
2022-05-14 11:50:40 +0800

    4.3 字符串切片语法糖

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
 fn main(){
-    let mut s = String::from("Hello World");
-    let index = first_world(&s);
+    let  s = String::from("Hello World");
+    let h1 = &s[0..5];
+    let w1 = &s[6..11];
+    //语法糖
+    let h2 = &s[..5];
+    let w2 = &s[6..];
+    let all = &s[..];
 
-    s.clear();
-    println!("{}",&index);
 }
 
-fn first_world(str : &String) ->usize{
-    let bytes = str.as_bytes();
-    // iter会返回一个迭代器
-    // enumerate会将迭代器包装为一个元组，1 索引， 2 元素的引用
-    for (i,&item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return  i;
-        }        
-    }
-    str.len()
-}
