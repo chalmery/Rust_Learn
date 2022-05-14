cb0254a94403fc9affd67c678dbfea6964507a02
chalmery <chaochaoycc@gmail.com>
2022-05-14 17:08:25 +0800

    5.5 关联方法

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,30 @@ struct Rectangle {
     length: u32,
     width: u32,
 }
-fn get_area(r : &Rectangle) -> u32 {
-    r.length * r.width
+
+impl Rectangle {
+    fn get_area(&self) -> u32 {
+        self.length * self.width
+    }
 }
+
+impl Rectangle {
+    fn square(size:u32) -> Rectangle {
+        Rectangle{
+            length :size,
+            width :size,
+        }
+    }
+}
+
 fn main() {
     let r = Rectangle{
         length:10,
         width:10,
     };
-    let area = get_area(&r);
+    let area = r.get_area();
     println!("{}",area);
 
-    println!("{:#?}",r)
+    let squ = Rectangle::square(10);
+    println!("{:#?}",squ)
 }
