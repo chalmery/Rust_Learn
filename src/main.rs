4120f47ba85cffe5e2add1bb678e6dc94934a17a
chalmery <chaochaoycc@gmail.com>
2022-05-14 11:37:51 +0800

    4.3 切片的一个小例子

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 fn main(){
-    let s = dangle();
+    let mut s = String::from("Hello World");
+    let index = first_world(&s);
+
+    s.clear();
+    println!("{}",&index);
 }
 
-fn dangle() -> &String {
-    let str = String::from("hello");
-    &str
+fn first_world(str : &String) ->usize{
+    let bytes = str.as_bytes();
+    // iter会返回一个迭代器
+    // enumerate会将迭代器包装为一个元组，1 索引， 2 元素的引用
+    for (i,&item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return  i;
+        }        
+    }
+    str.len()
 }
