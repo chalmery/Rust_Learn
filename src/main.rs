0ca2d8e2b824b61be59e0e99d1cc2f8dca5c7d57
chalmery <chaochaoycc@gmail.com>
2022-05-15 14:36:50 +0800

    8.3 Vector内存地址问题

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 fn main() {
-    let v = vec![1,2,3,4];
+    let mut v = vec![1,2,3,4];
     //获取值的引用,来访问值，这样不太好，可能越界
     let third  = &v[2];
+    v.push(5);
     println!("{}", third);
-    // get方法返回一个Option
-    match v.get(5) {
-        None => println!("Null"),
-        Some(num) => println!("num is :{}",num),
-    }
+    //因为在push之后，thired指向的堆地址可能是老的
 }
