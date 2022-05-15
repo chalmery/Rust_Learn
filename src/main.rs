82768beb6ee0bb7a25c3cfacefabcb1edeef42f4
chalmery <chaochaoycc@gmail.com>
2022-05-15 11:31:50 +0800

    7 mod

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,4 @@
+
 fn main(){
-    let v = Some(1);
-    match v {
-        Some(3)=> println!("3"),
-        _ => (),
-    }
-    //针对一种情况，可以使用if let
-    if let Some(3) = v {
-        println!("3")
-    }
 
 }
