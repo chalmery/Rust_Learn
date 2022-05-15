5d4a886df9c5d2659713566b4de8e138b0a49822
chalmery <chaochaoycc@gmail.com>
2022-05-15 11:38:36 +0800

    8.1 vector集合的定义

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+fn main() {
+    // let v : Vec<i32> = Vec::new();
+    //指定初始值的方式创建,这样v不需要指定类型，因为可以推断出来
+    // let v = vec![1,2,3];
+    let mut v = Vec::new();
+    v.push(1);
+    v.push(2);
+    v.push(3);
+    v.push(4);
 
-fn main(){
-
+    println!("{:?}",v);
 }
