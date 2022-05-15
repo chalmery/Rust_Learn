382f914df067d251b79751de4db8ab9db61b5088
chalmery <chaochaoycc@gmail.com>
2022-05-15 15:17:19 +0800

    8.6 字符串拼接

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 fn main() {
-    let str = String::from("hello,world");
-    let  str2 = "xxx".to_string();
-    println!("{}", str);
+    let mut s = String::from("hello");
+    s.push_str("aaa");
+    let s1 = String::from("hello");
+    let s2 = String::from("world");
+    let s3 = s1 + &s2;
+    println!("{}", s1);
+    println!("{}", s2);
+    println!("{}", s3);
+
+    let string = format!("{}-{}", s1, s2);
+    println!("{}", string);
 }
