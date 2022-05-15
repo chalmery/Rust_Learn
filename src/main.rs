b93a1d291c0a0f5ea6009011136dd7ddc950df4a
chalmery <chaochaoycc@gmail.com>
2022-05-15 21:31:00 +0800

    9.1 错误分类处理

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
     let f = File::open("hello.txt");
     // 处理Result
-    let f=  match f {
+    let f = match f {
         Ok(file) => file,
-        Err(error) => panic!("Error open File {:?}",error),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(error) => panic!("Error create File {:?}", error),
+            },
+            other_error => panic!("Error open File {:?}", other_error),
+        },
     };
-
-
-
 }
