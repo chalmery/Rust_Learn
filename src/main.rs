896a7d37af801507d6edee7ea1800d87935a7b28
chalmery <chaochaoycc@gmail.com>
2022-05-15 16:40:40 +0800

    8.7 HashMap判断是否存在key

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,15 @@ fn main() {
         Some(s) => println!("this is {}", s),
         None => println!("this is None"),
     }
-
     //遍历
     for (k, v) in &map {
         println!("key: {} , value: {}", k, v);
     }
+    //查看key是否存在
+    let e = map.entry(String::from("Green"));
+    println!("{:?}", e);//返回了一个枚举
+    // or instert表示，如果返回的值没有就插入
+    let vel = e.or_insert(100);
+    println!("{}", vel); // 方法返回值为当前value值的可变的引用 &mut
+
 }
