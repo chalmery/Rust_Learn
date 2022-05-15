f602e2a99e26a6c205f042d1383b6a1b284f18e1
chalmery <chaochaoycc@gmail.com>
2022-05-15 15:04:10 +0800

    8.5 声明字符串

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,5 @@
 fn main() {
-    let mut  v = vec![1,2,3,4];
-    // 遍历
-    for num in &v {
-        print!("{} ", num);
-    }
-    println!(" ");
-    // 遍历并修改，使用解引用的方式，表示：值的修改
-    for num in &mut v {
-        *num += 10;
-    }
-    for num in &v {
-        print!("{} ", num);
-    }
+    let str = String::from("hello,world");
+    let  str2 = "xxx".to_string();
+    println!("{}", str);
 }
