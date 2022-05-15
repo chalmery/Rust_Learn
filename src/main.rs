3de84a572ecbf7e194a85d017de1d071cc315e01
chalmery <chaochaoycc@gmail.com>
2022-05-15 10:25:01 +0800

    6.4 附带值的模式匹配

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+#[derive(Debug)] // 这样可以立刻看到州的名称
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -10,6 +17,9 @@ fn value_in_cents(coin: Coin) -> u8 {
         Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
 }
