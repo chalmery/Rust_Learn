5ceff11e71c2fdb5981aa6339f84897bb6f8c94e
chalmery <chaochaoycc@gmail.com>
2022-05-15 21:25:05 +0800

    9.1 错误Result处理

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use std::fs::File;
 
 fn main() {
-    // panic!("这是一个恐慌")
-    let v = vec![1,2,3];
+    let f = File::open("hello.txt");
+    // 处理Result
+    let f=  match f {
+        Ok(file) => file,
+        Err(error) => panic!("Error open File {:?}",error),
+    };
 
-    &v[99];
+
+
 }
