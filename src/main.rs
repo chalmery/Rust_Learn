9e8e70761f64372141da9c06b08904a33682a78e
chalmery <chaochaoycc@gmail.com>
2022-05-15 16:46:19 +0800

    8.7 HashMap例子

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,16 @@
 use std::collections::HashMap;
 
 fn main() {
+    let text = "Hello world This is Rust world";
     let mut map = HashMap::new();
-    map.insert(String::from("Blue"), 10);
-    map.insert(String::from("Yellow"), 50);
-    let blue_str = String::from("Blue");
-    let res = map.get(&blue_str);
 
-    match res {
-        Some(s) => println!("this is {}", s),
-        None => println!("this is None"),
+    //根据空格切分
+    for word in text.split_whitespace(){
+        let count = map.entry(word).or_insert(0);
+        *count +=1; //对值解引用然后+1
     }
-    //遍历
-    for (k, v) in &map {
-        println!("key: {} , value: {}", k, v);
-    }
-    //查看key是否存在
-    let e = map.entry(String::from("Green"));
-    println!("{:?}", e);//返回了一个枚举
-    // or instert表示，如果返回的值没有就插入
-    let vel = e.or_insert(100);
-    println!("{}", vel); // 方法返回值为当前value值的可变的引用 &mut
+    println!("{:#?}", map);
+
+    //HashMap默认使用了安全的Hash算法，能抵御DDOS等攻击，如果觉得太慢可以切换Hasher
 
 }
