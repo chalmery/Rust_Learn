fb5202bc8eaabd8f88953193f1367b52b1b6162a
chalmery <chaochaoycc@gmail.com>
2022-05-15 20:54:35 +0800

    9.1 错误

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,7 @@
-use std::collections::HashMap;
 
 fn main() {
-    let text = "Hello world This is Rust world";
-    let mut map = HashMap::new();
+    // panic!("这是一个恐慌")
+    let v = vec![1,2,3];
 
-    //根据空格切分
-    for word in text.split_whitespace(){
-        let count = map.entry(word).or_insert(0);
-        *count +=1; //对值解引用然后+1
-    }
-    println!("{:#?}", map);
-
-    //HashMap默认使用了安全的Hash算法，能抵御DDOS等攻击，如果觉得太慢可以切换Hasher
-
+    &v[99];
 }
