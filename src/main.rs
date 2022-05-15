4deb069152a8f0127adfe8fa0d0089d8e33e3136
chalmery <chaochaoycc@gmail.com>
2022-05-15 10:31:48 +0800

    6.5 针对一种情况使用if let

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,12 @@
-#[derive(Debug)] // 这样可以立刻看到州的名称
-enum UsState {
-    Alabama,
-    Alaska,
-    // --snip--
-}
-
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
-}
-
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => 1,
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("State quarter from {:?}!", state);
-            25
-        }
+fn main(){
+    let v = Some(1);
+    match v {
+        Some(3)=> println!("3"),
+        _ => (),
     }
+    //针对一种情况，可以使用if let
+    if let Some(3) = v {
+        println!("3")
+    }
+
 }
