b0f8756af71b7c4014765a7e8f24e7494ed92726
chalmery <chaochaoycc@gmail.com>
2022-05-15 16:31:32 +0800

    8.7 HashMap遍历

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,18 @@ use std::collections::HashMap;
 
 fn main() {
     let mut map = HashMap::new();
-    map.insert(String::from("Blue"),10);
-    map.insert(String::from("Yellow"),50);
-    let blue_str= String::from("Blue");
+    map.insert(String::from("Blue"), 10);
+    map.insert(String::from("Yellow"), 50);
+    let blue_str = String::from("Blue");
     let res = map.get(&blue_str);
 
     match res {
-        Some(s) => println!("this is {}",s),
+        Some(s) => println!("this is {}", s),
         None => println!("this is None"),
+    }
+
+    //遍历
+    for (k, v) in &map {
+        println!("key: {} , value: {}", k, v);
     }
 }
