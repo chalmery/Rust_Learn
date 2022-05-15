754e7676d48d2846d14c381ee350c7bc11236475
chalmery <chaochaoycc@gmail.com>
2022-05-15 10:08:07 +0800

    6.1 枚举定义

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,9 @@
 #[derive(Debug)]
-struct Rectangle {
-    length: u32,
-    width: u32,
-}
-
-impl Rectangle {
-    fn get_area(&self) -> u32 {
-        self.length * self.width
-    }
-}
-
-impl Rectangle {
-    fn square(size:u32) -> Rectangle {
-        Rectangle{
-            length :size,
-            width :size,
-        }
-    }
+enum IP_ADDRESS{
+    V4(String,u32),
+    V6,
 }
 
 fn main() {
-    let r = Rectangle{
-        length:10,
-        width:10,
-    };
-    let area = r.get_area();
-    println!("{}",area);
-
-    let squ = Rectangle::square(10);
-    println!("{:#?}",squ)
+    println!("{:?}",IP_ADDRESS::V6)
 }
