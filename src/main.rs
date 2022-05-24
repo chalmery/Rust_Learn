43e17c4d4ad2b3976d8f725d093af17a8933fa08
chalmery <chaochaoycc@gmail.com>
2022-05-24 21:16:55 +0800

    10.2 rust自带的泛型

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
 
 fn main() {
-    let number_list = vec![];
-    let largest:i32 = get_vec_largest(&number_list);
-    println!("{}", largest);
 }
-
-fn get_vec_largest<T: std::cmp::PartialOrd>(number_list :&[T])-> T{
-    let mut largest = number_list[0];
-    for &num in number_list {
-        if num>largest {
-            largest = num;
-        }
-    }
-    largest
+//rust自带的枚举类型
+enum Option<T> {
+    None,
+    Some(T),
+}
+// 多个泛型
+enum Result<T, E> {
+   Ok(T),
+   Err(E),
 }
