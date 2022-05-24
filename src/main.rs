0c733ed49eabf08a63e6578f542da26b54006f17
chalmery <chaochaoycc@gmail.com>
2022-05-24 20:53:26 +0800

    9 提取方法

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
-mod food;//声明模块
-use food::cake::Cake;
+
 fn main() {
-    let cake = Cake{
-        size :100
-    };
-    println!("#{:?}",cake);
+    let number_list = vec![];
+    let largest = get_vec_largest(&number_list);
+    println!("{}", largest);
+}
+
+fn get_vec_largest(number_list :&[i32])-> i32{
+    if number_list.len()==0 {
+        return 0;
+    }
+    let mut largest = number_list[0];
+    for &num in number_list {
+        if num>largest {
+            largest = num;
+        }
+    }
+    largest
 }
