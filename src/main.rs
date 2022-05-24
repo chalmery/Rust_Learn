97408f6d55cce1e9d4dff1839590bd6da753bd64
chalmery <chaochaoycc@gmail.com>
2022-05-24 21:08:41 +0800

    10.1 泛型的限制

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,16 @@
 
 fn main() {
     let number_list = vec![];
-    let largest = get_vec_largest(&number_list);
+    let largest:i32 = get_vec_largest(&number_list);
     println!("{}", largest);
 }
 
-fn get_vec_largest(number_list :&[i32])-> i32{
-    if number_list.len()==0 {
-        return 0;
-    }
+fn get_vec_largest<T: std::cmp::PartialOrd>(number_list :&[T])-> T{
     let mut largest = number_list[0];
     for &num in number_list {
         if num>largest {
             largest = num;
         }
     }
-    //上面的引用方式，或者下面的解引用方式
-    // for num in number_list {
-    //     if *num>largest {
-    //         largest = *num;
-    //     }
-    // }
     largest
 }
