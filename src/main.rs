0e87b8b1eb31ba339c7bb3f00d6696478810ba8f
chalmery <chaochaoycc@gmail.com>
2022-05-24 21:29:26 +0800

    10.3 rust泛型

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 
+struct Point<T> {
+    x: T,
+    y: T,
+}
+//表示在类型T上实现方法
+impl<T> Point<T> {
+    //相当于get，获取x的引用
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+//可以根据具体的类型实现方法,这样，只有i32的类型才你能调用这个
+impl Point<i32> {
+    fn x1(&self) -> &i32 {
+        &self.x
+    }
+}
+
 fn main() {
-}
-//rust自带的枚举类型
-enum Option<T> {
-    None,
-    Some(T),
-}
-// 多个泛型
-enum Result<T, E> {
-   Ok(T),
-   Err(E),
+    let p = Point{
+        x : 100,
+        y : 100,
+    };
+
+    println!("{}", p.x());
+
 }
