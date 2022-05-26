941dfc415d9652b39da9de488cc84bc23c186611
chalmery <chaochaoycc@gmail.com>
2022-05-26 11:08:10 +0800

    10 默认实现的方法，调用其他抽象方法，实现模板模式

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,23 @@
 pub trait Summary {
     //接口方法的默认实现
     fn summarize(&self) -> String{
-        String::from("Read more....")
+        format!("Read more....and author : {}",self.summarize_author())
     }
+
+
+    fn summarize_author(&self) -> String;
 }
 
 pub struct News {
     pub headline: String,
     pub location: String,
+    pub author: String,
 }
 
+//因为存在默认实现，这里不重写表示使用默认的逻辑
 impl Summary for News {
-    
+    fn summarize_author(&self) -> String {
+        format!("@{}",self.author)
+    }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     let n = News {
         headline: String::from("he"),
         location: String::from("lo"),
+        author: String::from("ycc"),
     };
 
     println!("{}", n.summarize());
